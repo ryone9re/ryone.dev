4b276847789afb9bda21f0dde769aa87fb46683b
ryone9re <ryoichi.kurimoto@gmail.com>
2025-04-27 04:45:25 +0000

    Add site URL

--- astro.config.js
+++ astro.config.js
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+	site: "https://ryone.dev",
 	server: { host: "127.0.0.1" },
 	integrations: [react(), sitemap()],
 });
